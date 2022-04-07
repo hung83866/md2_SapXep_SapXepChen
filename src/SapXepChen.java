e403c98c08dcabac71e87de5193b476491a3adaf
unknown <hung.pnpt93@gmail.com>
2022-04-07 10:10:41 +0700

    săp xep chen

--- src/SapXepChen.java
+++ src/SapXepChen.java
@@ -2,10 +2,10 @@ public class SapXepChen {
     static int[] list={3,5,1,67,5,2,34,346,41,3,5,724};
     public static void sapXepChen(int[] arr){
         int element , index;
-        for (int i = 1; i < list.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
             index = i;
-            element = list[i];
-            while (index>0&&element<list[i-1]){
+            element = arr[i];
+            while (index>0&&element<arr[index-1]){
                 arr[index]=arr[index-1];
                 index--;
             }
@@ -15,8 +15,8 @@ public class SapXepChen {
 
     public static void main(String[] args) {
         sapXepChen(list);
-        for (int e:list) {
-            System.out.println(e +"");
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(list[i]+" ");
         }
     }
 }
